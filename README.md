9afc3055797030c2e180aab67d7190166e216559
Hasan Raza <hasan.raza@blackfrosttechnologies.com>
2023-04-07 13:57:51 -0400

    boilerplate: tech stack & project setup

---
+++ README.md
@@ -0,0 +1,9 @@
+# Project: BrainStation's InStock
+
+<br/>
+
+### Frontend Tech Stack:
+
+- React + Vite
+- Tailwind CSS
+- Axios
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
+import { fileURLToPath, URL } from 'url'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: [
+      { find: '@', replacement: fileURLToPath(new URL('./src', import.meta.url)) },
+      { find: '@components', replacement: path.resolve(__dirname, './src/components') },
+      { find: '@pages', replacement: path.resolve(__dirname, './src/pages') },
+      { find: '@services', replacement: path.resolve(__dirname, './src/services') },
+      { find: '@utils', replacement: path.resolve(__dirname, './src/utils') },
+    ],
+  },
   plugins: [react()],
 })
