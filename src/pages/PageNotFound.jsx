9e48aad8039ad8eabaa49071e1a92822e48c284d
Hasan Raza <hasan.raza@blackfrosttechnologies.com>
2023-04-16 18:03:40 -0400

    refactor: NavBar & Footer styling

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function PageNotFound() {
   return (
-    <div>PageNotFound</div>
+    <div className='py-4 text-4xl section'>404 Page Not Found :( </div>
   )
 }
