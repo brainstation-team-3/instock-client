f747c1bfd3aad790ed0b2a5563c9a994dbd08485
Kaveri Mittal <kaverimittal@Shivams-MacBook-Air.local>
2023-04-16 00:49:51 -0400

    feat: footer responsive

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div>Footer rrrrrrrrr</div>
+    <>
+      <p className="pt-8 pb-16 h-16 w-auto bg-instock-light-grey text-center text-instock-cloud md:h-24 md:py-26 xl:h-6 xl:pt-48 xl:pb-20">
+        &copy; inStock.Inc.All Rights Reserved
+      </p>
+    </>
   )
 }
