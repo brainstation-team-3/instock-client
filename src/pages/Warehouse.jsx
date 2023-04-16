c54c5f0aabf901bd9198280b4305526e6a769c33
A-Trott <100259248+A-Trott@users.noreply.github.com>
2023-04-16 11:17:22 -0600

    Add new warehouse page hooks

--- src/pages/Warehouse.jsx
+++ src/pages/Warehouse.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import WarehouseList from '@components/Warehouse/WarehouseList'
+import { getWarehouses } from '@utils/helpers'
 
-export default function Warehouse() {
+function Warehouse() {
+
+  // in javascript, we have event loops i.e. if something
+  // doesn't get completed in first loop, it gets completed in
+  // second loop and so on and so forth
+  const [loading, setLoading] = useState(true)
+  const [warehouseList, setWarehouseList] = useState([])
+
+  useEffect(() => {
+    
+    getWarehouses().then((data) => {
+      setWarehouseList(data)
+      setLoading(false)
+      console.log(data)
+    })
+
+  }, [])
+
+  if (loading) {
+    return <div>Loading...</div>
+  }
 
   return (
-    <div>
-      Warehouse Page
+      <div>
+          Warehouse
+          <WarehouseList warehouseList={warehouseList} />      
     </div>
   )
 }
+
+export default Warehouse
