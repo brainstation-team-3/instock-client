cf0d112685a0b12df592da6526986d9d65f12e11
Hasan Raza <hasan.raza@blackfrosttechnologies.com>
2023-04-16 18:03:57 -0400

    fix: yarn, npm & vite config

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,18 @@
-import { fileURLToPath, URL } from 'url'
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react';
 import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
-    alias: [
-      { find: '@', replacement: fileURLToPath(new URL('./src', import.meta.url)) },
-      { find: '@assets', replacement: path.resolve(__dirname, './src/assets') },
-      { find: '@components', replacement: path.resolve(__dirname, './src/components') },
-      { find: '@pages', replacement: path.resolve(__dirname, './src/pages') },
-      { find: '@services', replacement: path.resolve(__dirname, './src/services') },
-      { find: '@utils', replacement: path.resolve(__dirname, './src/utils') },
-    ],
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@services': path.resolve(__dirname, './src/services'),
+      '@utils': path.resolve(__dirname, './src/utils'),
+    },
   },
   plugins: [react()],
 })
