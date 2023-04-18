1029e8bcfb3f024d8ade18991cf6e3835e86e5e1
A-Trott <100259248+A-Trott@users.noreply.github.com>
2023-04-18 06:29:07 -0600

    Add warehouse Detail route

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Warehouse from '@pages/Warehouse'
 import Inventory from '@pages/Inventory'
+import WarehouseDetail from '@pages/WarehouseDetail'
 import PageNotFound from '@pages/PageNotFound'
 import NavBar from '@components/NavBar/NavBar'
 import Footer from '@components/Footer/Footer'
@@ -14,6 +15,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Warehouse />} />
           <Route path='/warehouse' element={<Warehouse />} />
+          <Route path='/warehouse/:id' element={<WarehouseDetail />} />
           <Route path='/inventory' element={<Inventory />} />
           <Route path='*' element={<PageNotFound />} />
         </Routes>
