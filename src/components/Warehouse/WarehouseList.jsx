eb6da2e2a6b91279e1dcdc64acbfb82af6be245c
A-Trott <100259248+A-Trott@users.noreply.github.com>
2023-04-17 22:22:07 -0600

    add searchbar and button components

--- src/components/Warehouse/WarehouseList.jsx
+++ src/components/Warehouse/WarehouseList.jsx
@@ -1,18 +1,27 @@
 import React from 'react';
 import WarehouseListItem from './WarehouseListItem';
+import SearchBar from '@components/Searchbar/SearchBar';
+import { ButtonPrimary } from '@components/Button/Button';
 // import Table from '@components/Table/Table';
 
 function WarehouseList({ warehouseList }) {
 
   return (
-    <div className='divide-y divide-solid section'> 
-      <h1 className='text-2xl font-bold'>Warehouses</h1>
-      <div>SEARCH BAR</div>
+    <section className='divide-y divide-solid mx-3'> 
+      <div className='p-4 flex flex-col gap-4'>
+        <h1 className='text-mh1 font-semibold pt-2 '>Warehouses</h1>
+        <SearchBar />
+        <ButtonPrimary 
+          type='button'
+          link=''
+          text='+ Add New Warehouse'  
+        />
+      </div>
       {/* <Table /> */}
       {warehouseList.map((item) => (
         <WarehouseListItem key={item.id} item={item} />
         ))}
-    </div>
+    </section>
   )
 }
 
