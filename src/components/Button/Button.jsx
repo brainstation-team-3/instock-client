cea74def565911c1a02d1afe8c90e27dfc205e0e
A-Trott <100259248+A-Trott@users.noreply.github.com>
2023-04-18 02:38:20 -0600

    Turn of flex-shrink for tablet breakpoint

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,7 +1,7 @@
 import { Link } from 'react-router-dom'
 import React from 'react'
 
-const sharedStyle = "h-9 md:h-9.5 px-4 w-full md:w-fit rounded-3xl text-mh3 md:text-h";
+const sharedStyle = "h-9 md:h-9.5 px-4 w-full md:w-fit md:shrink-0 rounded-3xl text-mh3 md:text-h3";
 
 // to add render logic if button type is button or submit //
 // also need to add ButtonSecondary and ButtonDelete based off the style guide
