ec70e3a6587fb667bb3810728f591a80744c45a7
A-Trott <100259248+A-Trott@users.noreply.github.com>
2023-04-18 02:37:00 -0600

    Add table headers for tablet and desktop

--- src/components/Warehouse/WarehouseList.jsx
+++ src/components/Warehouse/WarehouseList.jsx
@@ -2,25 +2,48 @@ import React from 'react';
 import WarehouseListItem from './WarehouseListItem';
 import SearchBar from '@components/Searchbar/SearchBar';
 import { ButtonPrimary } from '@components/Button/Button';
-// import Table from '@components/Table/Table';
+import sort from '@/assets/icons/sort-24px.svg';
+
 
 function WarehouseList({ warehouseList }) {
 
   return (
-    <section className='divide-y divide-solid mx-3'> 
-      <div className='p-4 flex flex-col gap-4'>
-        <h1 className='text-mh1 font-semibold pt-2 '>Warehouses</h1>
+    <section className=' mx-3 md:mx-6'>
+      <div className='p-4 md:px-9 flex flex-col md:flex-row gap-y-4 md:items-center md:gap-y-0 md:gap-x-4'>
+        <h1 className='text-mh1 md:text-h1 font-semibold pt-2 md:mr-auto '>Warehouses</h1>
         <SearchBar />
-        <ButtonPrimary 
+        <ButtonPrimary
           type='button'
           link=''
-          text='+ Add New Warehouse'  
+          text='+ Add New Warehouse'
         />
       </div>
-      {/* <Table /> */}
-      {warehouseList.map((item) => (
-        <WarehouseListItem key={item.id} item={item} />
+      <div className=''>
+        <div className='hidden md:block md:box-border md:px-9 md:flex md:gap-4 md:items-center md:justify-between w-full'>
+          <div className='hidden md:block md:basis-2/12 md:py-2 md:text-h4'>
+            <span>WAREHOUSE</span>
+            <img src={sort} alt='sort' className='inline h-4 pl-1' />
+          </div>
+          <div className='hidden md:block md:basis-2/12 md:py-2 md:text-h4'>
+            <span>ADDRESS</span>
+            <img src={sort} alt='sort' className='inline h-4 pl-1' />
+          </div>
+          <div className='hidden md:block md:basis-2/12 md:py-2 md:text-h4'>
+            <span>CONTACT NAME</span>
+            <img src={sort} alt='sort' className='inline h-4 pl-1' />
+          </div>
+          <div className='hidden md:block md:basis-3/12 md:py-2 md:text-h4'>
+            <span>CONTACT INFORMATION</span>
+            <img src={sort} alt='sort' className='inline h-4 pl-1' />
+          </div>
+          <div className='hidden md:block md:basis-1/12 md:py-2 md:text-h4'>
+            <span>ACTIONS</span>
+          </div>
+        </div>
+        {warehouseList.map((item) => (
+          <WarehouseListItem key={item.id} item={item} />
         ))}
+      </div>
     </section>
   )
 }
