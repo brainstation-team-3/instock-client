4e09cc6ced7f39f15b4193c075b404163476c03c
A-Trott <100259248+A-Trott@users.noreply.github.com>
2023-04-17 23:03:37 -0600

    Change searchbar to form and icon -> button

--- src/components/Searchbar/SearchBar.jsx
+++ src/components/Searchbar/SearchBar.jsx
@@ -4,14 +4,16 @@ import SearchIcon from '@/assets/icons/search-24px.svg'
 
 function SearchBar() {
   return (
-    <div className="flex flex-row w-full p-2 border border-instock-cloud rounded-3xl">
+    <form className="flex flex-row w-full h-9 md:h-8.5 relative ">
       <input
-        className=" text-mh3 w-full"
+        className=" text-mh3 w-full px-4 border border-instock-cloud focus:outline-instock-indigo rounded-3xl"
         type="text"
         placeholder="Search..."
       />
+      <button className='absolute right-4 top-1.5'>
       <img className="" src={SearchIcon} alt="search-logo" />
-    </div>
+      </button>
+    </form>
   )
 }
 
