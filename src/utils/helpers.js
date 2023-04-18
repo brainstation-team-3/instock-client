8793af6051c9783781df0556788eda8ba3a0a77c
A-Trott <100259248+A-Trott@users.noreply.github.com>
2023-04-18 06:31:00 -0600

    Add warehouse Detail axios call function

---
+++ src/utils/helpers.js
@@ -0,0 +1,26 @@
+import axios from '@services/axios'
+import routes from '@services/routes.json'
+
+async function getWarehouses() {
+  try {
+    const response = await axios.get(routes.warehouse);
+    if (response.status === 200) {
+      return response.data
+    }
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+async function getWarehouse(id) {
+  try {
+    const response = await axios.get(`${routes.warehouse}/${id}`);
+    if (response.status === 200) {
+      return response.data
+    }
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export {getWarehouses, getWarehouse};
