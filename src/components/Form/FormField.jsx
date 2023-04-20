bb158e84cf478fb55fdb1f92ac155756323e5b56
Hasan Raza <hasan.raza@blackfrosttechnologies.com>
2023-04-20 02:29:31 -0400

    fix(ui): visual styles

--- src/components/Form/FormField.jsx
+++ src/components/Form/FormField.jsx
@@ -1,9 +1,8 @@
 const FormField = ({ field, value, onChange }) => {
   return (
     <input
-      className="input-text h-9 w-full mb-2 mt-1 border border-solid border-instock-cloud
-      placeholder:capitalize rounded-[1.25rem] placeholder-text-instock-cloud px-4
-      active:border-instock-indigo md:h-[2.375rem]"
+      className="input-text py-2 w-full mb-2 mt-1 border border-instock-cloud rounded-3xl
+      placeholder:capitalize placeholder-text-instock-cloud px-4 active:border-instock-indigo"
       type="text"
       placeholder={field.replace("_", " ")}
       value={value}
