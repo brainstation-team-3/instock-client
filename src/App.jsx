0b2395b558054371cd6fbceb5fb9bb9a1b195509
Hasan Raza <razahasan24@gmail.com>
2023-04-23 20:25:07 -0400

    feat(PTSEP3-12): edit warehouse component

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Inventory from '@pages/Inventory'
 import PageNotFound from '@pages/PageNotFound'
 import NavBar from '@components/NavBar/NavBar'
 import Footer from '@components/Footer/Footer'
-import NewWarehouse from '@pages/NewWarehouse'
+import AddEditWarehouse from '@pages/AddEditWarehouse.jsx'
 
 function App() {
   return (
@@ -16,7 +16,8 @@ function App() {
           <Route path='/' element={<Warehouse />} />
           <Route path='/warehouse' element={<Warehouse />} />
           <Route path='/inventory' element={<Inventory />} />
-          <Route path="/new-warehouse" element={<NewWarehouse />} />
+          <Route path="/new-warehouse" element={<AddEditWarehouse />} />
+          <Route path="/warehouse/:warehouseId/edit" element={<AddEditWarehouse />} />
           <Route path='*' element={<PageNotFound />} />
         </Routes>
       </div>
--- src/components/Form/FormField.jsx
+++ src/components/Form/FormField.jsx
@@ -1,10 +1,10 @@
-const FormField = ({ field, value, onChange }) => {
+const FormField = ({ field, value, onChange, id }) => {
   return (
     <input
-      className="input-text py-2 w-full mb-2 mt-1 border border-instock-cloud rounded-3xl
+      className="input-text py-2 w-full mb-2 mt-1 border border-instock-cloud rounded-3xl capitalize
       placeholder:capitalize placeholder-text-instock-cloud px-4 active:border-instock-indigo"
       type="text"
-      placeholder={field.replace("_", " ")}
+      placeholder={id ? value : field.replace("_", " ")}
       value={value}
       onChange={(e) => onChange(prevState => ({...prevState, [field]: e.target.value }))}
       required
