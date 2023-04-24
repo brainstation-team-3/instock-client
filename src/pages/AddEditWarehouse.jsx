5fc056a9591338af8d74e4d17b776e095f676e90
Hasan Raza <razahasan24@gmail.com>
2023-04-23 20:53:41 -0400

    fix(PTSEP3-12): div width on form; applied tailwind formatter

--- src/pages/AddEditWarehouse.jsx
+++ src/pages/AddEditWarehouse.jsx
@@ -32,6 +32,7 @@ export default function AddEditWarehouse() {
         delete data.id
         setFields(data)
       }
+
       updateFields()
     }
 
@@ -48,26 +49,41 @@ export default function AddEditWarehouse() {
     navigate('/warehouse')
   }
 
-    return (
-      <>
-        <div className="mx-4 rounded-md bg-white shadow-md mt-[-4.2rem] md:mt-[-6rem] md:mx-8 xl:mx-auto xl:max-w-7xl">
-          <div className="inline-flex w-full border-b pt-8 pb-6 pl-4 justify-left border-instock-cloud">
-            <img className='cursor-pointer' src={ArrowBackIcon} alt="back-arrow" onClick={() => navigate('/warehouse')}/>
-            <h3 className="pl-2 page-header capitalize">
-              {warehouseId ? 'edit warehouse' : 'add new warehouse'}
-            </h3>
-          </div>
-          <div className='md:flex'>
-            <form className='mb-6 rounded-b-md border-b pt-4 md:mb-0'
-                  onSubmit={submitHandler}
-            >
-              <div className="section md:mb-6 md:flex">
-                <div
-                  className="border-b p-4 border-instock-cloud mx-[-1rem] md:mx-0 md:my-4 md:border-r md:border-b-0 md:px-6 md:py-0">
-                  <h4 className="pb-5 subheader">Warehouse Details</h4>
-                  {warehouseFields
-                    .map((field, index) => (
-                      <label key={index}>
+  return (
+    <>
+      <div className="mx-4 rounded-md bg-white shadow-md mt-[-4.2rem] md:mt-[-6rem] md:mx-8 xl:mx-auto xl:max-w-7xl">
+        <div className="inline-flex w-full border-b pt-8 pb-6 pl-4 justify-left border-instock-cloud">
+          <img className='cursor-pointer' src={ArrowBackIcon} alt="back-arrow" onClick={() => navigate('/warehouse')}/>
+          <h3 className="pl-2 capitalize page-header">
+            {warehouseId ? 'edit warehouse' : 'add new warehouse'}
+          </h3>
+        </div>
+        <div className='md:flex'>
+          <form className='mb-6 w-full rounded-b-md border-b pt-4 md:mb-0'
+                onSubmit={submitHandler}
+          >
+            <div className="section md:mb-6 md:flex">
+              <div
+                className="border-b p-4 border-instock-cloud mx-[-1rem] md:mx-0 md:my-4 md:w-1/2 md:border-r md:border-b-0 md:px-6 md:py-0">
+                <h4 className="pb-5 subheader">Warehouse Details</h4>
+                {warehouseFields
+                  .map((field, index) => (
+                    <label key={index}>
+                      <span className="pb-1 capitalize label-btn">{field.replace("_", " ")}</span>
+                      <FormField field={field}
+                                 onChange={setFields}
+                                 value={fields[field]}
+                                 id={warehouseId}
+                      />
+                    </label>
+                  ))}
+              </div>
+              <div className="p-4 mx-[-1rem] md:mx-0 md:my-4 md:w-1/2 md:px-6 md:py-0">
+                <h4 className="pb-5 subheader">Contact Details</h4>
+                {contactFields
+                  .map((field, index) => (
+                    <div key={index}>
+                      <label>
                         <span className="pb-1 capitalize label-btn">{field.replace("_", " ")}</span>
                         <FormField field={field}
                                    onChange={setFields}
@@ -75,43 +91,27 @@ export default function AddEditWarehouse() {
                                    id={warehouseId}
                         />
                       </label>
-                    ))}
-                </div>
-                <div className="p-4 mx-[-1rem] md:mx-0 md:my-4 md:px-6 md:py-0">
-                  <h4 className="pb-5 subheader">Contact Details</h4>
-                  {contactFields
-                    .map((field, index) => (
-                      <div key={index}>
-                        <label>
-                          <span className="pb-1 capitalize label-btn">{field.replace("_", " ")}</span>
-                          <FormField field={field}
-                                     onChange={setFields}
-                                     value={fields[field]}
-                                     id={warehouseId}
-                          />
-                        </label>
-                      </div>
-                    ))}
-                </div>
+                    </div>
+                  ))}
               </div>
-              <div className="flex w-full items-center gap-4 px-6 py-4 bg-instock-light-grey md:justify-end">
-                <button type="reset"
-                        className="label-btn w-1/2 border rounded-full
-                      text-instock-cloud hover:border-instock-indigo py-2 hover:text-instock-indigo md:w-28 md:px-4
-                      duration-300 ease-in-out"
-                >
-                  Cancel
-                </button>
-                <button type="submit"
-                        className="label-btn w-1/2 py-2 rounded-full bg-instock-indigo capitalize text-instock-white
-                      hover:bg-instock-black md:w-36 md:px-4 md:mr-8 duration-300 ease-in-out"
-                >
-                  {warehouseId ? 'save' : '+ add warehouse'}
-                </button>
-              </div>
-            </form>
-          </div>
+            </div>
+            <div className="flex w-full items-center gap-4 px-6 py-4 bg-instock-light-grey md:justify-end">
+              <button type="reset"
+                      className="label-btn w-1/2 border rounded-full text-instock-cloud hover:border-instock-indigo
+                        py-2 hover:text-instock-indigo md:w-28 md:px-4 duration-300 ease-in-out"
+              >
+                Cancel
+              </button>
+              <button type="submit"
+                      className="label-btn w-1/2 py-2 rounded-full bg-instock-indigo capitalize text-instock-white
+                        hover:bg-instock-black md:w-36 md:px-4 md:mr-8 duration-300 ease-in-out"
+              >
+                {warehouseId ? 'save' : '+ add warehouse'}
+              </button>
+            </div>
+          </form>
         </div>
-      </>
-    )
+      </div>
+    </>
+  )
 }
