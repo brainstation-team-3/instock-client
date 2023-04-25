a9975696270ec9c0d4cae36a4f9187e89f683c0a
Hasan Raza <razahasan24@gmail.com>
2023-04-25 02:31:49 -0400

    feature(PTSEP3-13|PTSEP3-23): delete warehouse/inventory modal

---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,56 @@
+import { createPortal } from 'react-dom'
+import { deleteWarehouse } from '@utils/helpers.js'
+import closeIcon from '@assets/icons/close-24px.svg'
+
+const Modal = ({ type, id, name, open, onClose, setCurrentList }) => {
+  if (!open) return null
+
+  if (open) {
+    document.body.style.overflowY = 'hidden'
+  }
+
+  const deleteHandler = () => {
+    deleteWarehouse(id)
+      .then(() => {
+        setCurrentList((prevState) => prevState.filter((item) => item.id !== id))
+        onClose()
+      })
+  }
+
+  return createPortal(
+    <div key={id}
+         className='fixed top-0 right-0 bottom-0 left-0 h-screen items-center justify-center bg-black/40 z-[1000] md:flex'>
+      <div className='top-0 right-0 bottom-0 left-0 mx-auto flex h-screen max-w-3xl flex-col gap-8 bg-white px-4 pt-20 pb-8
+                      drop-shadow-xl md:relative md:h-fit md:rounded-md md:px-12 md:pt-12'>
+        <h3
+          className='capitalize page-header'>delete {name} {type === 'warehouse' ? 'warehouse' : 'inventory item'}?</h3>
+        <p className='flex-grow text-instock-black md:grow-0'>
+          {`Please confirm that you’d like to delete the ${name} 
+            ${type === 'warehouse' ? 'warehouse from the list of warehouses' : 'from the inventory list'}. 
+            You won’t be able to undo this action.`}
+        </p>
+        <div className='flex items-end justify-end gap-4'>
+          <button type='reset'
+                  onClick={onClose}
+                  className='label-btn w-1/2 border rounded-full text-instock-cloud capitalize hover:border-instock-indigo
+                             py-2 hover:text-instock-indigo md:w-28 md:px-4 duration-300 ease-in-out'>
+            cancel
+          </button>
+          <button type='submit'
+                  onClick={deleteHandler}
+                  className='label-btn w-1/2 py-2 rounded-full bg-instock-red capitalize text-instock-white
+                             hover:bg-instock-black md:w-28 md:px-4 duration-300 ease-in-out'>
+            delete
+          </button>
+        </div>
+        <img className='absolute top-5 right-5 h-6 w-6 cursor-pointer'
+             onClick={onClose}
+             src={closeIcon}
+             alt='close-icon' />
+      </div>
+    </div>,
+    document.getElementById('portal'),
+  )
+}
+
+export default Modal
--- src/components/Warehouse/WarehouseListItem.jsx
+++ src/components/Warehouse/WarehouseListItem.jsx
@@ -1,44 +1,49 @@
-import {useNavigate} from 'react-router-dom'
-import editIcon from '@/assets/icons/edit-24px.svg';
-import deleteIcon from '@/assets/icons/delete_outline-24px.svg';
-import chevron from '@/assets/icons/chevron_right-24px.svg';
+import { useNavigate } from 'react-router-dom'
+import editIcon from '@/assets/icons/edit-24px.svg'
+import deleteIcon from '@/assets/icons/delete_outline-24px.svg'
+import chevron from '@/assets/icons/chevron_right-24px.svg'
 
-function WarehouseListItem({item}) {
-  const navigate = useNavigate();
+function WarehouseListItem({ item, onDelete, setCurrentWarehouse }) {
+  const navigate = useNavigate()
 
-  const {id, warehouse_name, address, city, country, contact_name, contact_phone, contact_email} = item;
+  const { id, warehouse_name, address, city, country, contact_name, contact_phone, contact_email } = item
+
+  const deleteHandler = () => {
+    setCurrentWarehouse(item)
+    onDelete(true)
+  }
 
   return (
-    <div key={id} className="grid grid-cols-2 border-b bg-white px-6 py-8 shadow-md border-instock-cloud
-    last:rounded-b-md md:grid-cols-5 md:items-center md:px-10 md:last:border-b-0"
-    >
-      <div className="mb-4 md:mb-0">
-        <p className="text-sm font-bold uppercase text-instock-slate md:hidden">warehouse</p>
-        <span className="flex items-center gap-1 py-1">
-           <p className="cursor-pointer font-bold capitalize text-instock-indigo">{warehouse_name}</p>
-           <img className="mt-1" src={chevron} alt="chevron-icon"/>
+    <div key={id} className='grid grid-cols-2 border-b bg-white px-6 py-8 shadow-md border-instock-cloud
+                             last:rounded-b-md md:grid-cols-5 md:items-center md:px-10 md:last:border-b-0'>
+      <div className='mb-4 md:mb-0'>
+        <p className='text-sm font-bold uppercase text-instock-slate md:hidden'>warehouse</p>
+        <span className='flex items-center gap-1 py-1'>
+           <p className='cursor-pointer font-bold capitalize text-instock-indigo'>{warehouse_name}</p>
+           <img className='mt-1' src={chevron} alt='chevron-icon' />
          </span>
       </div>
-      <div className="col-start-1 mb-4 w-4/5 break-words md:col-start-2 md:mb-0">
-        <p className="text-sm font-bold uppercase text-instock-slate md:hidden">address</p>
-        <p className="capitalize">{address}, {city}, {country}</p>
+      <div className='col-start-1 mb-4 w-4/5 break-words md:col-start-2 md:mb-0'>
+        <p className='text-sm font-bold uppercase text-instock-slate md:hidden'>address</p>
+        <p className='capitalize'>{address}, {city}, {country}</p>
       </div>
-      <div className="col-start-2 row-start-1 mb-4 w-4/5 break-words md:col-start-3 md:mb-0">
-        <p className="text-sm font-bold uppercase text-instock-slate md:hidden">contact name</p>
-        <p className="capitalize">{contact_name}</p>
+      <div className='col-start-2 row-start-1 mb-4 w-4/5 break-words md:col-start-3 md:mb-0'>
+        <p className='text-sm font-bold uppercase text-instock-slate md:hidden'>contact name</p>
+        <p className='capitalize'>{contact_name}</p>
       </div>
-      <div className="col-start-2 mb-4 break-words md:col-start-4 md:mb-0">
-        <p className="text-sm font-bold uppercase text-instock-slate md:hidden">contact information</p>
+      <div className='col-start-2 mb-4 break-words md:col-start-4 md:mb-0'>
+        <p className='text-sm font-bold uppercase text-instock-slate md:hidden'>contact information</p>
         <p>{contact_phone}</p>
         <p>{contact_email}</p>
       </div>
       <div
-        className="col-span-2 flex items-center justify-between md:col-span-1 md:mr-2 md:mb-0 md:justify-end md:gap-4">
-        <img className="cursor-pointer md:w-7" src={deleteIcon} alt="delete-icon"/>
+        className='col-span-2 flex items-center justify-between md:col-span-1 md:mr-2 md:mb-0 md:justify-end md:gap-4'>
+        <img onClick={deleteHandler} className='cursor-pointer md:w-7' src={deleteIcon}
+             alt='delete-icon' />
         <img onClick={() => navigate(`/warehouse/${id}/edit`)}
-             className="cursor-pointer justify-self-end md:w-7"
+             className='cursor-pointer justify-self-end md:w-7'
              src={editIcon}
-             alt="delete-icon"
+             alt='delete-icon'
         />
       </div>
     </div>
