c649f39054afbadfef09bc64b604854fb1508df1
ElizabethAwesome <elizabeth2awesome@gmail.com>
2023-04-26 19:07:39 -0700

    feat: inventory item detail component layout

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import PageNotFound from '@pages/PageNotFound'
 import AddEditWarehouse from '@pages/AddEditWarehouse'
 import Footer from '@components/Footer/Footer'
 import NewInventoryItem from '@components/Inventory/NewInventoryItem'
+import WarehouseInventoryList from '@components/Warehouse/WarehouseInventoryList'
 
 function App() {
   return (
@@ -22,6 +23,7 @@ function App() {
           <Route path="/new-warehouse" element={<AddEditWarehouse />} />
           <Route path="/warehouse/:warehouseId/edit" element={<AddEditWarehouse />} />
           <Route path="/new-inventory-item" element={<NewInventoryItem />} />
+          <Route path='/warehouse/:warehouseId/inventory' elementh={<WarehouseInventoryList />} />
           <Route path='*' element={<PageNotFound />} />
         </Routes>
       </div>
